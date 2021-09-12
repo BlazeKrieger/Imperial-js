afe2a011ad931fc4c9d24c5cd2755839ebb11d6a
David Tsang <david.tsang19@imperial.ac.uk>
2021-09-12 20:42:25 +0200

    implemented dates that can be scrolled to through clicking

--- script/timeline.js
+++ script/timeline.js
@@ -2,22 +2,24 @@ const timeline = Object.create(null);
 
 const el = (id) => document.getElementById(id);
 
-const tag = (tag) => document.querySelector(tag);
+const tag = (tag) => document.getElementsByTagName(tag);
 
 const cn = (cl) => document.getElementsByClassName(cl);
 
 var milestones = cn("milestone");
 
+
+
 window.addEventListener("DOMContentLoaded", function () {
     timeline.init();
 });
 
-timeline.init = function () {
-    prompt("clicked on body");
-    for (let i = 0, mlen = milestones.length; i < mlen; i++) {
-        tag(li).href = "#" + String(milestones[i]);
+timeline.init = function () {  
+    for (var i in milestones) {
+        cn("date")[i].href = "#" + String(milestones[i].innerText);
+        milestones[i].setAttribute("id",String(milestones[i].innerText));
     }
-}
+};
 
 
 
