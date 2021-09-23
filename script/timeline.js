b5381706997e174a378bcf9edc628a7820122811
David Tsang <david.tsang19@imperial.ac.uk>
2021-09-23 22:53:57 +0200

    fonts, index page, progress with timeline interaction

--- script/timeline.js
+++ script/timeline.js
@@ -12,15 +12,30 @@ var milestones = cn("milestone");
 
 window.addEventListener("DOMContentLoaded", function () {
     timeline.init();
+    timeline.scrollAnimation();
 });
 
-timeline.init = function () {  
+timeline.init = function () {
     for (var i in milestones) {
         cn("date")[i].href = "#" + String(milestones[i].innerText);
-        milestones[i].setAttribute("id",String(milestones[i].innerText));
+        milestones[i].setAttribute("id", String(milestones[i].innerText));
     }
 };
 
-
+timeline.scrollAnimation = function () {
+    for (;;) {
+        alert("loop")
+        for (var i in milestones) {
+            var segment = el(String(milestones[i].innerText)).getBoundingClientRect().top;
+            if (segment <= 0) {
+                milestones[i].setAttribute("class", String("active"));
+            }
+            else {
+                milestones[i].removeAttribute("class");
+            }
+            alert(cl("active"));
+        }
+    }
+}
 
 export default Object.freeze(timeline);
